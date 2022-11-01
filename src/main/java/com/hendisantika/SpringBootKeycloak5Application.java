5f554a87c4bbc7abc2774716eed28f58a773dc46
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-01 07:59:24 +0700

    #1 - Initial Commit :green_book: :whale:

---
+++ src/main/java/com/hendisantika/SpringBootKeycloak5Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloak5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloak5Application.class, args);
+    }
+
+}
