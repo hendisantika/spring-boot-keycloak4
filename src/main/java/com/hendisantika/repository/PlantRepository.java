d6df9c69e03dfae4b6edea75a11827954f229c16
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-01 08:05:31 +0700

    #5 - Add Plant Repository :green_book: :whale:

---
+++ src/main/java/com/hendisantika/repository/PlantRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Plant;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak5
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/1/22
+ * Time: 08:05
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PlantRepository extends CrudRepository<Plant, Long> {
+}
