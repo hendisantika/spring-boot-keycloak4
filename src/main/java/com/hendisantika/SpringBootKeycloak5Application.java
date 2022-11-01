b990edae0edcfc65e6e8cf9ecf587778164ea6bb
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-01 08:08:35 +0700

    #8 - Add KeycloakConfigResolver :green_book: :whale:

--- src/main/java/com/hendisantika/SpringBootKeycloak5Application.java
+++ src/main/java/com/hendisantika/SpringBootKeycloak5Application.java
@@ -1,7 +1,10 @@
 package com.hendisantika;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootKeycloak5Application {
@@ -10,4 +13,8 @@ public class SpringBootKeycloak5Application {
         SpringApplication.run(SpringBootKeycloak5Application.class, args);
     }
 
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 }
