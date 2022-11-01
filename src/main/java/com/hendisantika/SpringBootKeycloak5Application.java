2e2981cdbf1149702b605d33d3540fe93101f104
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-01 09:57:03 +0700

    #16 - Add init Data :green_book: :whale2:

--- src/main/java/com/hendisantika/SpringBootKeycloak5Application.java
+++ src/main/java/com/hendisantika/SpringBootKeycloak5Application.java
@@ -1,10 +1,17 @@
 package com.hendisantika;
 
+import com.hendisantika.entity.Plant;
+import com.hendisantika.repository.PlantRepository;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootKeycloak5Application {
@@ -16,5 +23,17 @@ public class SpringBootKeycloak5Application {
     @Bean
     public KeycloakConfigResolver KeycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    public ApplicationRunner run(PlantRepository plantRepository) throws Exception {
+        return (ApplicationArguments args) -> {
+            List<Plant> plants = Arrays.asList(
+                    new Plant("subalpine fir", "abies lasiocarpa", "pinaceae"),
+                    new Plant("sour cherry", "prunus cerasus", "rosaceae"),
+                    new Plant("asian pear", "pyrus pyrifolia", "rosaceae")
+            );
+            plantRepository.saveAll(plants);
+        };
     }
 }
